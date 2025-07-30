1fe0ba035884190928d8b9c327996e439e4419b5
tavaresdiony <dionathatavares19@gmail.com>
2025-07-30 15:46:03 +0000

    finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,8 +8,8 @@ int main() {
 
 //cadastro da primeira carta
 
-    printf("Primeira carta\n");
-    printf("População: ");
+    printf("Primeira carta \n");
+    printf("\nPopulação: ");
     scanf("%d", &populacao1);
 
     printf("Área: ");
@@ -23,19 +23,19 @@ int main() {
 
 // resultados primeira carta
 
-    printf("CARTA 01\n");
-    printf("População: %d\n", populacao1);
-    printf("Área: %f\n", area1);
-    printf("%f\n", PIB1);
-    printf("%d\n", pontosturisticos1);
+    printf("\n CARTA 01: \n");
+    printf("\n-População: %d\n", populacao1);
+    printf("-Área: %.4f\n", area1);
+    printf("-PIB: %.4f\n", PIB1);
+    printf("-Pontos turísticos: %d\n", pontosturisticos1);
 
     int populacao2, pontosturisticos2;
     float area2, PIB2;
 
  //cadastro da segunda carta
 
-    printf("Segunda carta\n");
-    printf("População: ");
+    printf("\nSegunda carta\n");
+    printf("\nPopulação: ");
     scanf("%d", &populacao2);
 
     printf("Área: ");
@@ -49,11 +49,11 @@ int main() {
 
     // resultados segunda carta
 
-    printf("CARTA 02\n");
-    printf("População: %d\n", populacao2);
-    printf("Área: %f\n", area2);
-    printf("%f\n", PIB2);
-    printf("%d\n", pontosturisticos2);
+    printf("\n CARTA 02: \n");
+    printf("\n-População: %d\n", populacao2);
+    printf("-Área: %.4f\n", area2);
+    printf("-PIB: %.4f\n", PIB2);
+    printf("-Pontos turístico: %d\n", pontosturisticos2);
 
     return 0;
 }
